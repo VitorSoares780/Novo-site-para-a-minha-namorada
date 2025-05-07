6a2251a1fc2f7490e7cdc49770770c6b917499f1
VitorSoares780 <vitorsoares.ch033@academico.ifsul.edu.br>
2025-05-07 00:22:31 -0300

    updt lista suspensa

--- paginas/script2.js
+++ paginas/script2.js
@@ -206,4 +206,7 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 
     galleryContainer.innerHTML = htmlContent;
+});document.querySelector('.dropdown').addEventListener('click', function() {
+    const menu = this.querySelector('.dropdown-menu');
+    menu.style.display = menu.style.display === 'block' ? 'none' : 'block';
 });
